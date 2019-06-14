eb4c51daa9f5e3ee4bb1e6ec623e88595dbe61fd
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-06-13 18:17:21 -0700

    Updated batch files to reflect firmware name change
    I updated the name of the firmware file so also had to update batch name to match. I also updated the folder with the batch files and has to refected in the batch to go to the correct path

---
+++ MyDefaultParametersList.md
@@ -0,0 +1,2 @@
+MyDefaultparameterslist.md is a list of all the parameters in the "custom_306_super_patcher_MavicPro_MPP_dji_system.bin" 
+This is mainly for reference. It includes all of the min/max parameter ranges as well.
--- auto_install.bat
+++ auto_install.bat
@@ -1,4 +1,4 @@
-Echo off
+@Echo off
 Echo Starting Super Patcher for Mavic Pro and Mavic Pro Platinum!
 pause
 Echo This is a Flight Controller Patch for DJI Mavic Pro and Pro Plantinum. It will not work on any other drones!
@@ -15,15 +15,15 @@ Echo that has a STOCK 01.04.0300 firmware installed!
 PAUSE
 ECHO If your firmware is a mixed with the flight controller from 01.03.0700-
 Echo **DO NOT PROCEED before reflashing a clean stock version of 01.04.0300 using DUMLdore!**
-ECHO Flash stock 01.04.0300 twice to be sure the entire firware has installed 
+ECHO Flash stock 01.04.0300 **TWICE** to be sure the entire firware has installed 
 pause
 Echo This modification will automatically:
 ECHO	[1] Remove height limits from the firmware.
 ECHO	[2] Turn off aircraft side nfz restrictions.
-ECHO	[3] Enable Gallileo satellite reception.
-ECHO 	[4] Extends the parameter range that yaw movement limit can be set later in Assistant 1.1.2 by the user. 
-Echo (No change in yaw speed till set by user)  
-ECHO *These modifications are not optional and to be undone the user must reflash the original firmware 01.04.0300 firmware.*
+ECHO	[3] Enables Gallileo satellite reception.
+ECHO	[4] Enables slower and smoother yaw speed than any other current method 
+Echo 		(No change in yaw speed untill set by user in Assitant 1.1.2, normal mode yaw speed and cinematic yaw speed can be modified in DJI Go 4)  
+ECHO To undo this modification reflash stock 01.04.0300 using DUMLdore
 ECHO Additional modifications can be enabled by the user using Assistant 2 1.1.2 in debug mode. Some parameter ranges have been expanded.
 pause
 ECHO **The purpose of this modification is to allow experienced and law abiding citizens to unlock their Mavic Pro without the need to contact DJI to fly their aircraft.
@@ -33,5 +33,5 @@ Echo The pilot in command is always resposible for flight safety and following l
 Echo This removes some of the "idiot-proofing" designed into DJI products so take extra care to know your local laws before each flight**
 Echo ***DO NOT PROCEED IF YOU DONT UNDERSTAND***
 Pause
-cd MavicPro_Patched_FC
+cd TOOLS
 Call run_me.bat
