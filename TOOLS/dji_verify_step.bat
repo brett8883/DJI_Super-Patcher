cce4bc3d984e4ca40a9a481e1133d27cdf956c52
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-06-29 12:19:12 -0700

    updated batches

--- TOOLS/dji_verify_step.bat
+++ TOOLS/dji_verify_step.bat
@@ -36,13 +36,27 @@ echo please load the file
 echo "custom_306_super_patcher_MavicPro_MPP_dji_system.bin" into DUMLdore 
 start DUMLdoreV3.exe
 ECHO then click "flash firmware" in DUMLdore
-ECHO Please allow DUMLdore to do its thing and do not disconnect the Mavic untill DUMLdore says it is ok to
+ECHO Please allow DUMLdore to do its thing and do not disconnect the Mavic untill DUMLdore says it is ok to 
+echo once DUMLdore is finished please restart the aircraft
+echo Once aircraft has restarted fully and connected to PC 
+echo you may continue
+pause
+Echo You have completed the Super-Patcher process!
+Echo To verify Super-Patcher was successful 
+Echo I will open the NLD app which will show you the flight controller number 
+Echo for your aircraft. If Super-Patcher was successful this will show your 
+Echo Flight Controller number as 03.02.44.10
+Echo *************************************************************************************
+Echo If NLD app shows your Flight Controller # is not 03.02.44.10 
+ECHO then Super-Patcher was not successful and you should restart this process from the begining 
+Echo Ready to start NLD and check FC number?
 pause 
-echo once DUMLdore is finished please restart the aircraft 
-ECHO and then connect to Assistant 2 and use the simulator to ensure proper working order in the sim before testing outside 
-Echo You may now modify any paramters you'd like using Assistant 2 1.1.2 in debug mode
+start NLD.exe
+ECHO Once you have verified Super-Patcher was successful you can optionally 
+ECHO aconnect to Assistant 2 and use the simulator to ensure proper working order in the sim before testing outside 
+Echo You may now also modify any paramters you'd like using Assistant 2 1.1.2 in debug mode
 echo ******************************************************************************************************
-Echo You have completed the patching operation. Please note all settings and parameters on the Mavic are likely now reset
+Echo You have completed the patching operation. Please note all settings and parameters are likely now reset
 Echo remember to check your RTH altitude and such.
 Echo The aircraft may ask you to recalibrate sensors but if it does not then it isn't neccesary
 Echo Bye 
