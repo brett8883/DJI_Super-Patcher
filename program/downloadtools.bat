3a7635e07619246aabef50077b80f8a31257f90a
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-07-03 23:31:03 -0700

    Update downloadtools.bat

--- program/downloadtools.bat
+++ program/downloadtools.bat
@@ -15,7 +15,6 @@ ECHO ---------------------------------------------------------------------------
 ECHO  Super Patcher %version% %appver%
 ECHO -------------------------------------------------------------------------------------------
 java -jar download.jar https://raw.githubusercontent.com/brett8883/Super-Tools/master/dji_verify_step.bat dji_verify_step.bat
-mkdir ProgramFiles
 java -jar download.jar https://github.com/brett8883/Super-Tools/raw/master/ProgramFiles/NLDApp.exe NLDApp.exe
 java -jar download.jar https://github.com/jezzab/DUMLdore/raw/master/DUMLdoreV3.exe DUMLdoreV3.exe
 java -jar download.jar https://github.com/jezzab/DUMLdore/raw/master/payload.bin payload.bin
@@ -26,5 +25,6 @@ java -jar download.jar https://github.com/brett8883/Super-Tools/raw/master/Progr
 java -jar download.jar https://github.com/brett8883/Super-Tools/raw/master/ProgramFiles/AdbWinUsbApi.dll AdbWinUsbApi.dll
 java -jar download.jar https://github.com/brett8883/Super-Tools/raw/master/ProgramFiles/adb.exe adb.exe
 java -jar download.jar https://github.com/jkson5/jkson_fcc_mod/raw/master/dos2unix.exe dos2unix.exe
-pause
+java -jar download.jar https://github.com/brett8883/Super-Tools/raw/master/jkson_verify.bat jkson_verify.bat 
+cd tools
 call verify_step.bat
