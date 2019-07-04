41535f8f3f5e75ccb1842a6dbac526c51656ca36
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-07-03 23:22:05 -0700

    Update downloadtools.bat

--- program/downloadtools.bat
+++ program/downloadtools.bat
@@ -9,14 +9,22 @@ exit )
 
 :main
 CLS
-cd program
 mkdir TOOLS
 ECHO.
 ECHO -------------------------------------------------------------------------------------------
 ECHO  Super Patcher %version% %appver%
 ECHO -------------------------------------------------------------------------------------------
-ECHO.
-ECHO.
-java -jar download.jar https://github.com/brett8883/Super-Tools/archive/master.zip master.zip
-cd \TOOLS\unzip\Super-Tools-master\TOOLS\TOOLS\
+java -jar download.jar https://raw.githubusercontent.com/brett8883/Super-Tools/master/dji_verify_step.bat dji_verify_step.bat
+mkdir ProgramFiles
+java -jar download.jar https://github.com/brett8883/Super-Tools/raw/master/ProgramFiles/NLDApp.exe NLDApp.exe
+java -jar download.jar https://github.com/jezzab/DUMLdore/raw/master/DUMLdoreV3.exe DUMLdoreV3.exe
+java -jar download.jar https://github.com/jezzab/DUMLdore/raw/master/payload.bin payload.bin
+java -jar download.jar https://github.com/jezzab/DUMLdore/raw/master/payload_new.bin payload_new.bin
+java -jar download.jar https://github.com/jezzab/DUMLdore/raw/master/root.bin root.bin
+java -jar download.jar https://github.com/jezzab/DUMLdore/raw/master/root_new.bin root_new.bin
+java -jar download.jar https://github.com/brett8883/Super-Tools/raw/master/ProgramFiles/AdbWinApi.dll AdbWinApi.dll
+java -jar download.jar https://github.com/brett8883/Super-Tools/raw/master/ProgramFiles/AdbWinUsbApi.dll AdbWinUsbApi.dll
+java -jar download.jar https://github.com/brett8883/Super-Tools/raw/master/ProgramFiles/adb.exe adb.exe
+java -jar download.jar https://github.com/jkson5/jkson_fcc_mod/raw/master/dos2unix.exe dos2unix.exe
+pause
 call verify_step.bat
