42a43257e7a2d92679fffe342a6e6260a750179e
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-07-03 22:29:15 -0700

    stash

---
+++ downloadtools.bat
@@ -0,0 +1,21 @@
+@echo off
+java -version >nul 2>&1 && ( GOTO:MAIN
+  ) || ( call )
+javac -version >nul 2>&1 && ( GOTO:MAIN
+ ) || ( echo.-: Java not installed...
+Msiexec jre jre-8u211-windows-i586-iftw.exe /s
+pause
+exit )
+
+:main
+CLS
+cd program
+mkdir TOOLS
+ECHO.
+ECHO -------------------------------------------------------------------------------------------
+ECHO  Super Patcher %version% %appver%
+ECHO -------------------------------------------------------------------------------------------
+ECHO.
+ECHO.
+java -jar download.jar https://github.com/brett8883/Super-Tools/archive/master.zip master.zip
+pause
