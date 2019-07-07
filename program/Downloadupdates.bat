67c606ffce6b0b839101498a8c72816fc1aed928
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-07-06 18:15:45 -0700

    Update Downloadupdates.bat

--- program/Downloadupdates.bat
+++ program/Downloadupdates.bat
@@ -1,7 +1,7 @@
 @echo off
 rmdir /Q /S nonemptydir TOOLS
 cls
-wget https://github.com/brett8883/Super-Tools/raw/master/downloadtools.bat
-cls
+wget https://github.com/brett8883/Super-Tools/raw/%appver%/downloadtools.bat
 pause
+cls
 call downloadtools.bat
