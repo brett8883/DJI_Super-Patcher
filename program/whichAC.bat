1f9c3240ab68e0e1b0acd33934ddbc346f237529
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-07-07 19:23:26 -0700

    Fixed bug with spark fw

--- program/whichAC.bat
+++ program/whichAC.bat
@@ -67,7 +67,7 @@ set AC=Spark
 set fc=03.02.44.09
 set stock=1.00.0900 
 set vt=1
-set fw=https://github.com/brett8883/Super-Firmware_Cache/raw/master/Spark_Super_Patcher_03.02.44.09_dji_sytem.bin
+set fw=hhttps://github.com/brett8883/Super-Firmware_Cache/raw/master/dji_system_wm100_0306_03.02.43.09_dji_system.bin
 set fw2=Spark_Super_Patcher_03.02.44.09_dji_sytem.bin
 cls
 wget %fw%
