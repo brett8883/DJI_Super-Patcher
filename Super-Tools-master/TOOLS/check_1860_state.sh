76c6a8f93552530f6faa64630395f5cd24e3b296
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-07-09 12:02:33 -0700

    delte

--- Super-Tools-master/TOOLS/check_1860_state.sh
+++
@@ -1,8 +0,0 @@
-#/system/bin/sh 
-/system/bin/check_1860_state.sh& 
-busybox ping -c 1 -w 1800 192.168.41.2 
-sleep 5 
-while : 
-do 
-break 
-done 
--- Super-Tools-master/startup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cls
-SETLOCAL EnableDelayedExpansion
-set appver=2.0
-call whichAC.bat
