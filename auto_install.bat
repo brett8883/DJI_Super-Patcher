fe7e946170c9ddd9a731f86018f7233c771a7afb
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-07-09 11:22:39 -0700

    Update auto_install.bat

--- auto_install.bat
+++ auto_install.bat
@@ -1,7 +1,11 @@
 @echo off
 cls
-cd program
+rmdir /Q /S nonemptydir Super-Tools-master
 del startup.bat
-wget https://raw.githubusercontent.com/brett8883/Super-Tools/master/startup.bat
+cls
+wget https://github.com/brett8883/Super-Tools/archive/master.zip
+unzip master.zip
+del master.zip
+cd Super-Tools-master
 call startup.bat 
 
