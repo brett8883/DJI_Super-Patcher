aa0a1f996874ba0870091c91577eea485d1ebfd5
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-07-08 20:50:31 -0700

    Update whichAC.bat

--- program/whichAC.bat
+++ program/whichAC.bat
@@ -11,16 +11,16 @@ ECHO ---------------------------------------------------------------------------
 ECHO	1) Mavic Pro 1 / Mavic Platinum / Mavic Artic White
 ECHO	2) Spark
 ECHO	3) Phantom 4 Pro
-ECHO	4) Phantom 4 Pro V2
+ECHO	4) Phantom 4 Standard
 ECHO	5) Phantom 4 Advanced
-ECHO	6) Phantom 4 Standard
+ECHO	6) Phantom 4 Pro V2
 ECHO	7) Inspire 2
 ECHO.
 choice /C 1234567 /D 1 /T 99 /M "Please select connected device"
 If Errorlevel 7 goto Sorry
 If Errorlevel 6 goto Sorry
-If Errorlevel 5 goto Sorry
-If Errorlevel 4 goto Sorry
+If Errorlevel 5 goto p4a
+If Errorlevel 4 goto p4s
 If Errorlevel 3 goto p4p
 If Errorlevel 2 goto SPK
 If Errorlevel 1 goto MP
@@ -90,6 +90,48 @@ set stock=1.05.0600
 set vt=2
 set fw=https://github.com/brett8883/Super-Firmware_Cache/raw/master/P4P_Super-Patcher_bin/P4P_wm331_Super_Patcher_0306_03.02.44.31_dji_system.bin
 set fw2=P4P_wm331_Super_Patcher_0306_03.02.44.31_dji_system.bin
+cls
+wget %fw%
+copy *.bin ..
+del *.bin
+cd ..
+cd program
+echo %vt%
+echo %AC%
+echo %stock%
+echo %fc%
+call phantom_verify.bat
+
+:p4a
+@echo off
+cls
+set AC=P4A
+set fc=03.02.35.32
+set stock=1.00.0128
+set vt=2
+set fw=https://github.com/brett8883/Super-Firmware_Cache/raw/master/P4Advanced_Super_Patcher%20FC/P4Advanced_wm332_Super_Patcher_FC_0306_03.02.35.32_dji_system.bin
+set fw2=P4Advanced_wm332_Super_Patcher_FC_0306_03.02.35.32_dji_system.bin
+cls
+wget %fw%
+copy *.bin ..
+del *.bin
+cd ..
+cd program
+echo %vt%
+echo %AC%
+echo %stock%
+echo %fc%
+call phantom_verify.bat
+
+:p4s
+@echo off
+cls
+set AC=P4Standard
+set fc=03.02.44.33
+set stock=2.00.0700 
+set vt=2
+set fw=https://github.com/brett8883/Super-Firmware_Cache/raw/master/P4Standard_Super_Patcher_FC/P4Standard_wm330_0306_03.02.44.33_Super_Patcher_dji_system_.bin
+set fw2=P4Standard_wm330_0306_03.02.44.33_Super_Patcher_dji_system_.bin
 cls
 wget %fw%
 copy *.bin ..
