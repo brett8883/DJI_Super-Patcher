51dccadb33ddde895f7bb99b9c089dcada2d4d7d
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-07-15 16:34:49 -0600

    beta

---
+++ Super-Tools-1.8_beta/startup.bat
@@ -0,0 +1,10 @@
+@echo off
+ECHO -------------------------------------------------------------------------------------------
+ECHO Super Patcher
+ECHO By Brett8883
+ECHO -------------------------------------------------------------------------------------------
+adb kill-server
+cls
+SETLOCAL EnableDelayedExpansion
+set appver=1.8_Beta
+call whichAC.bat
--- auto_install.bat
+++ auto_install.bat
@@ -10,9 +10,9 @@ Echo ***************************************************************************
 Echo *****************************************************************************************************
 Echo Please wait while I set things up. This wont take long...
 Timeout 5
-wget https://github.com/brett8883/Super-Tools/archive/master.zip
-unzip master.zip
-del master.zip
-cd Super-Tools-master
+wget https://github.com/brett8883/Super-Tools/archive/1.8_beta.zip
+unzip 1.8_beta.zip
+del 1.8_beta.zip
+cd Super-Tools-1.8_beta
 call startup.bat 
 
