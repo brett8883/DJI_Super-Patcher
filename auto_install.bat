62bc14b8e26ff33dc30746489ddfd332e8134a3e
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-08-04 15:09:57 -0600

    Update auto_install.bat

--- auto_install.bat
+++ auto_install.bat
@@ -5,12 +5,12 @@ adb kill-server
 taskkill /im adb.exe
 cls
 ECHO -------------------------------------------------------------------------------------------
-ECHO SUPER-PATCHER Set Up Widget 1.7.3
+ECHO SUPER-PATCHER Set Up Widget 1.7.4
 echo By Brett8883
 ECHO -------------------------------------------------------------------------------------------
 ECHO -------------------------------------------------------------------------------------------
 Echo Please wait while I set things up. This wont take long...
-Timeout 3
+Timeout 2
 wget https://github.com/brett8883/Super-Tools/archive/master.zip
 unzip -o master.zip
 del master.zip
